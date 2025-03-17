f220c160d5cf84397c75c574dd57103ddd547739
Marina <cher.marina03@gmail.com>
2025-03-17 09:13:25 +0300

    Удалены лишние файлы

--- .section.php
+++
@@ -1,9 +0,0 @@
-<?
-$sSectionName = 'Главная';
-$arDirProperties = array(
-	'title' => 'Test task',
-	'description' => 'Test task bitrix',
-	'keywords' => 'test task bitrix',
-	'robots' => 'index, follow'
-);
-?>
